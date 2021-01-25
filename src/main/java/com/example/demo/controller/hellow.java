783bcb25784d0e6739caa2f084b2c9bc33b00766
nibabaai <164468353@qq.com>
2021-01-25 22:29:44 +0800

    add

---
+++ src/main/java/com/example/demo/controller/hellow.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/first")
+@RestController
+public class hellow {
+
+    @GetMapping(value = "/hello")
+    public String helloWord(){
+
+        var a = "aaa";
+        var b = a;
+
+        var obj = new myObj();
+        obj.setName(a);
+
+
+
+        return obj.getname();
+
+    }
+
+}
+
+
---
+++ src/main/java/com/example/demo/controller/myObj.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+public class myObj {
+    private String name;
+
+    public String getname(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
